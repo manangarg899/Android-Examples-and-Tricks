7e982cd3b70b219fae06e455fdd8072a7e6acc00
Manan Garg <manangarg899@gmail.com>
2017-03-17 20:03:56 +0530

    Implementation of splash screen

---
+++ SplashScreen/app/src/main/java/com/example/manan/splashscreen/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.example.manan.splashscreen;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private final int TIME_OF_SPLASH_SCREEN = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, TIME_OF_SPLASH_SCREEN);
+    }
+}
