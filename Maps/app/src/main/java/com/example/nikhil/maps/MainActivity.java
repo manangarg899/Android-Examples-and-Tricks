f92e38886dca26e074ee48c5a447476567c45d9e
Nikhil Bansal <nikhil97.nb@gmail.com>
2017-06-29 11:53:19 +0530

    Google Maps API to show Marker

---
+++ Maps/app/src/main/java/com/example/nikhil/maps/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.nikhil.maps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        supportMapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng india = new LatLng(20.593684,78.962880);
+        googleMap.addMarker(new MarkerOptions().position(india)
+        .title("Marker in India"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(india));
+    }
+}
